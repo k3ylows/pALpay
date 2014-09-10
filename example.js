22a1cae03dc9140425bfae91b4a80075e1e1fd7f
k3ylows <k3ylows@gmail.com>
2014-09-10 16:21:59 -0700

    test

---
+++ example.js
@@ -0,0 +1,17 @@
+<script>
+var skipToConfig =
+{
+    "settings": {
+        "skipTo": {
+            "headings": "h1, h2, h3, h4",
+            "landmarks": "[role=banner], [role=navigation], [role=main], [role=search]",
+            "ids": "#SkipToA1, #SkipToA2",
+            "customClass": "MyClass",
+            "accesskey": "0",
+            "wrap": "true",
+            "visibility": "onfocus",
+            "attachElement": ".MyCustomClass" // or "attachElement": "#MyCustomId"
+        }
+    }
+};
+</script>
